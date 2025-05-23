0b5356ea7092d8305e02f87b3fcbd49d9259e6a5
jlcambraia <joaoluiz.cambraia@gmail.com>
2025-05-23 10:29:31 -0300

    feat: permite retornar o owner

--- models/article.js
+++ models/article.js
@@ -41,7 +41,6 @@ const articleSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "user",
       required: true,
-      select: false,
     },
   },
   { versionKey: false }
